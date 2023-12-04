3e79d216156785f198726e6f85928058c276f6ee
voideus <bikash.tamang@wesionary.team>
2023-12-04 13:18:41 +0545

    chore: populate dasboard with seeded data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,31 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+interface CardWrapperProps {
+  totalPaidInvoices: number | string;
+  totalPendingInvoices: number | string;
+  numberOfInvoices: number | string;
+  numberOfCustomers: number | string;
+}
+
+export default async function CardWrapper({
+  totalPaidInvoices,
+  totalPendingInvoices,
+  numberOfInvoices,
+  numberOfCustomers,
+}: CardWrapperProps) {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
